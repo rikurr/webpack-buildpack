7926ad149911dda64a22bd96ac1c158b27b87ec9
riku0329 user name <riku03290@gmail.com>
2020-06-01 16:37:20 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './js/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env']],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpeg|jpg|svg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'img/[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozipeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style/main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
